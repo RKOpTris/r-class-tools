617799bd416ef2bcc798a8d263b93bd12c068f1c
RKOpTris <mukappa@Mus-MacBook-Pro.local>
2023-12-04 11:00:29 +0000

    Updated tth_split.R

--- tth_split.R
+++ tth_split.R
@@ -1,16 +1,11 @@
-# quickly split data randomly into train, test and holdback sets using predefined proportions 
+# quickly split data randomly into train, test and holdout sets using a specific variable and predefined proportions 
 
-tth_split <- function(df, var, test_prop = 0.3, holdback_test_prop = 1/3, shuffle_train = F){
+tth_split <- function(df, var, test_prop = 0.15, holdout_prop = 0.15, shuffle_train = F){
   tth_split <- split(df, df[[var]])
-  split_data <- function(df, test_prop, holdback_test_prop){
-    test <- sample(nrow(df), floor(test_prop * nrow(df)))
-    holdback <- sample(test, floor(holdback_test_prop * length(test)))
-    df$.TTH <- "train"
-    df$.TTH[test] <- "test"
-    df$.TTH[holdback] <- "holdback"
+  tth_data <- lapply(tth_split, function(df){
+    df$.TTH <- sample(c("train", "test", "holdout"), nrow(df), replace = T, prob = c(train_prop, test_prop, holdout_prop))
     df
-  }
-  tth_data <- lapply(tth_split, split_data, test_prop, holdback_test_prop)
+  })
   tth_data <- do.call(rbind, tth_data)
   row.names(tth_data) <- 1:nrow(tth_data)
   tth_data <- tth_data %>% split(tth_data$.TTH)
@@ -21,6 +16,6 @@ tth_split <- function(df, var, test_prop = 0.3, holdback_test_prop = 1/3, shuffl
   tth_data
 }
 
-# tth_split(iris, "Species", 0.3, 1/3)
+# tth_split(iris, "Species", 0.15, 0.15)
 # used in conjunction with list_to_objects from https://github.com/RKOpTris/r-useful-funs
 # tth_split %>% list_to_objects("iris")
